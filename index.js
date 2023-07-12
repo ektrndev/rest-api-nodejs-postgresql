2f9ae13e0b832d401927d173ab6043bd0d3312e4
ektrndev <135712702+ektrndev@users.noreply.github.com>
2023-07-12 17:17:25 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const PORT = 8080
+const app = express();
+const userRouter = require('./routes/user.routes')
+const postRouter = require('./routes/post.routes')
+
+
+app.use(express.json())
+app.use('/api', userRouter)
+app.use('/api', postRouter)
+
+app.listen(PORT, () => { console.log('Server is gone on 8080')});
